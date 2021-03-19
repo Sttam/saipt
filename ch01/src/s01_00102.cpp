db4133955ff122fc587e4cfe77d5534227cd0bf7
Sttam <mathew.john@hotmail.com>
2021-03-19 22:09:50 +0530

    .

---
+++ ch01/src/s01_00102.cpp
@@ -0,0 +1,35 @@
+/*
+
+
+   Date           : 2020/05/26
+   Author         : Matts
+
+   Purpose        : Checking to see the environment, compiler, cmake works for you.
+
+   Instructions   : 
+                       ......
+                       1. run ./s01_00102
+
+   Quiz yourself  :
+                      :) I will ask you when I take the class
+                      :) There could be more questions. Let us limit it here.
+
+*/
+
+#include<iostream>
+int return_magic();
+
+int main( int argc, char* argv[], char *envp[]){
+   
+    auto x = return_magic();
+
+
+    return EXIT_SUCCESS;
+}
+
+int return_magic(){
+
+    int i{100};
+
+    return i;
+}
---
+++ ch01/src/s01_00103.cpp
@@ -0,0 +1,32 @@
+/*
+
+
+   Date           : 2020/05/26
+   Author         : Matts
+
+   Purpose        : Given a number x, find whether it is exactly divisble by 4
+
+   Instructions   : 
+                       ......
+                       1. run ./s01B00104
+
+   Quiz yourself  :
+                      1.  How do you solve it
+                      2.  Give one way to solve it.
+                      3.  Give one more way to solve it.
+                      4.  Give a loop to find it. // What is the issue with it?
+
+                      :) Ask yourself is the solution right
+*/
+
+#include <iostream>
+
+
+int main( int argc, char* argv[], char *envp[]){
+
+
+    std::cout << "\nYour turn to solve it :)\n";
+
+     
+    return EXIT_SUCCESS;
+}
