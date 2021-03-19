1b92c5ff046cae457812c83902ac4be8df24c7a5
Sttam <mathew.john@hotmail.com>
2021-03-19 23:49:46 +0530

    .

---
+++ ch22/inc/s22_00101.hpp
@@ -0,0 +1,23 @@
+#ifndef s22_00101_hpp
+#define s22_00101_hpp
+namespace saip {
+
+class Shape {
+public:
+  Shape(int w, int h) : width{w}, height{h} {}
+  void setWidth(int w) { width = w; }
+  void setHeight(int h) { height = h; }
+
+protected:
+  int width;
+  int height;
+};
+
+// Derived class
+class Rectangle : public Shape {
+public:
+  Rectangle(int w, int h) : Shape{w, h} {};
+  int getArea() { return (width * height); }
+};
+} // namespace saip
+#endif
---
+++ ch22/src/s22_00101.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <s22_00101.hpp>
+
+using namespace std;
+using namespace saip;
+int main() {
+    Rectangle r{20,30};
+
+    cout << "Area of the rectangle " << r.getArea()<< endl;
+}
